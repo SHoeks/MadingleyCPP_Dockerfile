cc744c44dccc451e5f18f533902ea0496cfb7d2f
SHoeks <36075931+SHoeks@users.noreply.github.com>
2020-01-29 11:06:24 +0100

    Update setupDocker.sh

--- setupDocker.sh
+++ setupDocker.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# require correct path to input_dir
-read -p 'Specify Madingley input data directory: ' input_dir
-printf "\n"
-
 # require correct docker image id
 docker images
 printf "\n"
 read -p 'Specify docker image name: ' image_name
 printf "\n"
 echo Running $image_name docker image... 
-echo Mounting $input_dir in docker image...
 printf "\n"
 
-docker run -v $input_dir:/mnt/MadingleyData-master -it $image_name /bin/bash
+docker run -it $image_name /bin/bash
